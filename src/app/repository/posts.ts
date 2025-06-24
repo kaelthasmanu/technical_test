fe2c1554b1013fed80fe7a2b48314eaa84473aa0
Manuel <manuelalberto.gorrin@gmail.com>
2025-06-24 15:18:16 -0400

    Define Post interfaces and update repository functions for better type safety

--- src/app/repository/posts.ts
+++ src/app/repository/posts.ts
@@ -1,6 +1,7 @@
+import { Post, CreatePostParams } from "../types/server.interfaces";
 import { query } from "./config.axios";
 
-export async function fetchPosts(): Promise<any[]> {
+export async function fetchAllPosts(): Promise<Post[]> {
     const response = await query.get('/posts');
     if (response.status < 200 || response.status >= 300) {
         throw new Error('Failed to fetch posts');
@@ -8,10 +9,33 @@ export async function fetchPosts(): Promise<any[]> {
     return response.data;
 }
 
-export async function fetchPost(): Promise<any> {
+export async function fetchPost(): Promise<Post> {
     const response = await query.get('/posts/1');
     if (response.status < 200 || response.status >= 300) {
         throw new Error('Failed to fetch post');
     }
     return response.data;
+}
+
+export async function createPost(post: CreatePostParams): Promise<Post> {
+    const response = await query.post('/posts', post);
+    if (response.status < 200 || response.status >= 300) {
+        throw new Error('Failed to create post');
+    }
+    return response.data;
+}
+
+export async function updatePost(post: Partial<Post>): Promise<Post> {
+    const response = await query.patch(`/posts/${post.id}`, post);
+    if (response.status < 200 || response.status >= 300) {
+        throw new Error('Failed to patch post');
+    }
+    return response.data;
+}
+
+export async function deletePost(id: number): Promise<void> {
+    const response = await query.delete(`/posts/${id}`);
+    if (response.status < 200 || response.status >= 300) {
+        throw new Error('Failed to delete post');
+    }
 }
